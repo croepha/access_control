3459140d93358c6d7f3020680abfba955228eb05
David Butler <davidbutler@MacBook-Pro.cronet>
2019-10-25 15:19:17 -0500

    .

---
+++ database.cpp
@@ -0,0 +1,397 @@
+
+
+#include <sqlite3.h>
+#include <SPI.h>
+#include <FS.h>
+#include <SPIFFS.h>
+
+typedef uint64_t u64;
+typedef uint32_t u32;
+typedef uint16_t u16;
+typedef uint8_t  u8;
+
+typedef int64_t  s64;
+typedef int32_t  s32;
+typedef int16_t  s16;
+typedef int8_t   s8;
+
+extern u64 rtc_offset;
+extern u8 rfid[10];
+extern u8 rfid_len;
+
+sqlite3 *db = 0;
+
+void db_add_user(const char* user_name);
+void db_get_delete_list(char* tmp_buf, size_t tmp_buf_len);
+bool db_check_and_log_access();
+void db_list_log();
+void db_prune();
+void db_del_user(u64 uid);
+void db_init();
+
+inline static u64 get_adjusted_time() { return time(0) + rtc_offset; }
+
+#define db_assert_ok(m_r) __db_assert_ok(m_r, __FILE__, __LINE__)
+inline static void __db_assert_ok(int r, const char*file, int line) {
+    if (r != SQLITE_OK) {
+        Serial.printf("ERROR: %s:%d: SQL ERROR:%d: %s\n", file, line, r, sqlite3_errmsg(db));
+        abort();
+    }
+}
+
+inline static void debug_print_rfid() {
+    for (int i=0;i<rfid_len;i++) {
+        Serial.printf("%02x ", rfid[i]);
+    }
+    Serial.printf("\n");
+}
+
+
+void db_set_rtc_offset() {
+    static const char*q = "SELECT \"log\".\"when\" FROM \"log\" ORDER BY \"log\".\"id\" DESC LIMIT 1";
+    sqlite3_stmt *stmt;
+    auto r1 = sqlite3_prepare_v2(db, q, -1, &stmt, 0);
+    db_assert_ok(r1);
+    assert(stmt);
+    
+    auto r2 = sqlite3_step(stmt);
+    if (r2 == SQLITE_DONE) {
+        rtc_offset = 0;
+    } else {
+        assert(r2 == SQLITE_ROW);
+        rtc_offset = sqlite3_column_int64(stmt, 0) - time(0);
+        auto r3 = sqlite3_step(stmt);
+        assert(r3 == SQLITE_DONE);  
+    }
+    
+    auto r4 = sqlite3_finalize(stmt);
+    db_assert_ok(r4);
+}
+
+
+void db_add_user(const char* user_name) {
+    Serial.printf("DEBUG: db_add_user %d `%s'\n", rfid_len, user_name);
+    debug_print_rfid();
+    
+    u64 now = get_adjusted_time();
+    {
+        static const char*q = "UPDATE \"user\" SET \"active\" = FALSE WHERE \"rfid\" = ?";
+        sqlite3_stmt *stmt;
+        auto r1 = sqlite3_prepare_v2(db, q, -1, &stmt, 0);
+        db_assert_ok(r1);
+        assert(stmt);
+        auto r3 = sqlite3_bind_blob(stmt, 1, rfid, rfid_len, SQLITE_TRANSIENT);
+        db_assert_ok(r3);
+        auto r2 = sqlite3_step(stmt);
+        assert(r2 == SQLITE_DONE);
+        auto r4 = sqlite3_finalize(stmt);
+        db_assert_ok(r4);
+    }
+    {
+        static const char*q = "INSERT INTO \"user\" (\"rfid\", \"name\", \"last_seen\", \"active\") VALUES (?, ?, ?, TRUE)";
+        sqlite3_stmt *stmt;
+        auto r1 = sqlite3_prepare_v2(db, q, -1, &stmt, 0);
+        db_assert_ok(r1);
+        assert(stmt);
+        auto r3 = sqlite3_bind_blob(stmt, 1, rfid, rfid_len, SQLITE_TRANSIENT);
+        db_assert_ok(r3);
+        auto r4 = sqlite3_bind_text(stmt, 2, user_name, -1, SQLITE_TRANSIENT);
+        db_assert_ok(r4);
+        auto r5 = sqlite3_bind_int64(stmt, 3, now);
+        db_assert_ok(r5);
+        auto r2 = sqlite3_step(stmt);
+        assert(r2 == SQLITE_DONE);
+        auto r6 = sqlite3_finalize(stmt);
+        db_assert_ok(r6);
+    }
+    
+    {
+        auto id = sqlite3_last_insert_rowid(db);
+        static const char*q = "INSERT INTO \"log\" (\"user_id\", \"when\", \"type\") VALUES (?, ?, 'N')";
+        sqlite3_stmt *stmt;
+        auto r1 = sqlite3_prepare_v2(db, q, -1, &stmt, 0);
+        db_assert_ok(r1);
+        assert(stmt);
+        auto r3 = sqlite3_bind_int64(stmt, 1, id);
+        db_assert_ok(r3);
+        auto r4 = sqlite3_bind_int64(stmt, 2, now);
+        db_assert_ok(r4);
+        auto r2 = sqlite3_step(stmt);
+        assert(r2 == SQLITE_DONE);
+        auto r6 = sqlite3_finalize(stmt);
+        db_assert_ok(r6);    
+    }
+}
+
+
+void db_get_delete_list(char* tmp_buf, size_t tmp_buf_len) {
+    
+    size_t tmp_buf_used = 0;
+    
+    Serial.printf("DEBUG: user list:\n");
+    static const char*q = "SELECT \"id\", \"name\", \"last_seen\" FROM \"user\" WHERE \"active\" = TRUE";
+    sqlite3_stmt *stmt;
+    auto r1 = sqlite3_prepare_v2(db, q, -1, &stmt, 0);
+    db_assert_ok(r1);
+    assert(stmt);
+    
+    for(;;) {
+        auto r3 = sqlite3_step(stmt);
+        if (r3 == SQLITE_DONE) break;
+        assert(r3==SQLITE_ROW);
+        
+        auto new_used = snprintf(
+            tmp_buf + tmp_buf_used, 
+            tmp_buf_len - tmp_buf_used,
+            "<p><a href=/del?id=%s>%s:%s</a></p>",
+            sqlite3_column_text(stmt, 0),
+            sqlite3_column_text(stmt, 2),
+            sqlite3_column_text(stmt, 1)
+            );
+        if (new_used < 0) {
+            Serial.printf("delete list overflow\n");
+            break;
+        }
+        tmp_buf_used += new_used;
+    }
+    
+    auto r6 = sqlite3_finalize(stmt);
+    db_assert_ok(r6);
+    
+}
+
+bool db_check_and_log_access() {
+    Serial.printf("DEBUG: check and log access %d\n", rfid_len);
+    debug_print_rfid();
+    
+    u64 now = get_adjusted_time();
+    int id = -1;
+    {
+        static const char*q = "SELECT \"user\".\"id\" FROM \"user\" WHERE \"user\".\"active\" = TRUE AND \"user\".\"rfid\" = ?";
+        sqlite3_stmt *stmt;
+        auto r1 = sqlite3_prepare_v2(db, q, -1, &stmt, 0);
+        db_assert_ok(r1);
+        assert(stmt);
+        auto r5 = sqlite3_bind_blob(stmt, 1, rfid, rfid_len, SQLITE_TRANSIENT);
+        db_assert_ok(r5);
+        
+        
+        
+        auto r2 = sqlite3_step(stmt);
+        if (r2 == SQLITE_DONE) {
+            Serial.printf("DEBUG: denied\n");
+            auto r7 = sqlite3_finalize(stmt);
+            db_assert_ok(r7);
+            return 0;
+        } 
+        assert(r2 == SQLITE_ROW);
+        id = sqlite3_column_int64(stmt, 0);
+        auto r3 = sqlite3_step(stmt);
+        assert(r3 == SQLITE_DONE);  
+        auto r6 = sqlite3_finalize(stmt);
+        db_assert_ok(r6);
+    }
+    
+    {
+        static const char*q = "INSERT INTO \"log\" (\"user_id\", \"when\", \"type\") VALUES (?, ?, 'A')";
+        sqlite3_stmt *stmt;
+        auto r1 = sqlite3_prepare_v2(db, q, -1, &stmt, 0);
+        db_assert_ok(r1);
+        assert(stmt);
+        auto r3 = sqlite3_bind_int64(stmt, 1, id);
+        db_assert_ok(r3);
+        auto r4 = sqlite3_bind_int64(stmt, 2, now);
+        db_assert_ok(r4);
+        auto r2 = sqlite3_step(stmt);
+        assert(r2 == SQLITE_DONE);
+        auto r6 = sqlite3_finalize(stmt);
+        db_assert_ok(r6);    
+    }
+    {
+        static const char*q = "UPDATE \"user\" SET \"last_seen\" = ? WHERE id = ?";
+        sqlite3_stmt *stmt;
+        auto r1 = sqlite3_prepare_v2(db, q, -1, &stmt, 0);
+        db_assert_ok(r1);
+        assert(stmt);
+        auto r3 = sqlite3_bind_int64(stmt, 1, now);
+        db_assert_ok(r3);
+        auto r4 = sqlite3_bind_int64(stmt, 2, id);
+        db_assert_ok(r4);
+        auto r2 = sqlite3_step(stmt);
+        assert(r2 == SQLITE_DONE);
+        auto r6 = sqlite3_finalize(stmt);
+        db_assert_ok(r6);    
+    }
+    
+    Serial.printf("DEBUG: granted\n");
+    return 1;
+    
+}
+
+
+
+void db_list_log() {
+    Serial.printf("DEBUG: log list:\n");
+    static const char*q = "SELECT \"user\".\"name\", \"log\".\"when\" FROM \"user\", \"log\" WHERE \"log\".\"user_id\" = \"user\".\"id\" ORDER BY \"log\".\"when\" DESC";
+    sqlite3_stmt *stmt = 0;
+    int r1 = sqlite3_prepare_v2(db, q, -1, &stmt, 0);
+    db_assert_ok(r1);
+    assert(stmt);
+    Serial.printf("DEBUG: 1\n");
+    
+    Serial.printf("Debug Access: COUNT %d \n",
+                  sqlite3_column_count(stmt)
+                  );
+    for(;;) {
+        Serial.printf("DEBUG: 2\n");
+        auto r3 = sqlite3_step(stmt);
+        if (r3 == SQLITE_DONE) break;
+        assert(r3==SQLITE_ROW);
+        Serial.printf("DEBUG: 3\n");
+        
+        Serial.printf("Debug Access3: `%s' `%s' \n",
+                      sqlite3_column_text(stmt, 0),
+                      sqlite3_column_text(stmt, 1)
+                      );
+        Serial.printf("%lld\n", sqlite3_column_int64(stmt, 1));
+        
+        Serial.printf("Debug Access4: `%s' `%s' \n",
+                      sqlite3_column_text(stmt, 1),
+                      sqlite3_column_text(stmt, 0)
+                      );
+        
+        Serial.printf("Debug Access: %lld `%s' \n",
+                      sqlite3_column_int64(stmt, 1),
+                      sqlite3_column_text(stmt, 0)
+                      );
+    }
+    
+    auto r6 = sqlite3_finalize(stmt);
+    db_assert_ok(r6);
+    
+}
+
+void db_prune() {
+    Serial.printf("DEBUG: prune \n");
+    u64 now = get_adjusted_time();
+    {
+        static const char*q = "DELETE FROM \"log\" WHERE \"when\" < ?;";
+        sqlite3_stmt *stmt;
+        auto r1 = sqlite3_prepare_v2(db, q, -1, &stmt, 0);
+        db_assert_ok(r1);
+        assert(stmt);
+        auto r3 = sqlite3_bind_int64(stmt, 1, (s64)now - 60*60*24*5);
+        db_assert_ok(r3);
+        auto r2 = sqlite3_step(stmt);
+        assert(r2 == SQLITE_DONE);
+        auto r4 = sqlite3_finalize(stmt);
+        db_assert_ok(r4);    
+    }
+    {
+        static const char*q = "DELETE FROM \"user\" WHERE \"last_seen\" < ?;";
+        sqlite3_stmt *stmt;
+        auto r1 = sqlite3_prepare_v2(db, q, -1, &stmt, 0);
+        db_assert_ok(r1);
+        assert(stmt);
+        auto r3 = sqlite3_bind_int64(stmt, 1, (s64)now - 60*60*24*30*6);
+        db_assert_ok(r3);
+        auto r2 = sqlite3_step(stmt);
+        assert(r2 == SQLITE_DONE);
+        auto r4 = sqlite3_finalize(stmt);
+        db_assert_ok(r4);    
+    }
+}
+
+void db_del_user(u64 uid) {
+    Serial.printf("DEBUG: del user %lld\n", uid);
+    static const char*q = "UPDATE \"user\" SET \"active\" = FALSE WHERE \"id\" = ?;";
+    sqlite3_stmt *stmt;
+    auto r1 = sqlite3_prepare_v2(db, q, -1, &stmt, 0);
+    db_assert_ok(r1);
+    assert(stmt);
+    auto r3 = sqlite3_bind_int64(stmt, 1, uid);
+    db_assert_ok(r3);
+    auto r2 = sqlite3_step(stmt);
+    assert(r2 == SQLITE_DONE);
+    auto r4 = sqlite3_finalize(stmt);
+    db_assert_ok(r4);
+}
+
+
+static int callback(void *data, int argc, char **argv, char **azColName) {
+    int i;
+    for (i = 0; i<argc; i++){
+        Serial.printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
+    }
+    Serial.printf("\n");
+    return 0;
+}
+
+void debug_exec(const char*sql) {
+    Serial.printf("Execing debug SQL: %s\n", sql);  
+    char *zErrMsg = 0;
+    int rc = sqlite3_exec(db, sql, callback, 0, &zErrMsg);
+    if (rc != SQLITE_OK) {
+        Serial.printf("SQL error: %s\n", zErrMsg);
+        sqlite3_free(zErrMsg);
+    } else {
+        Serial.printf("Operation done successfully\n");
+    }
+    
+}
+
+
+void db_init() {
+    sqlite3_initialize();
+    
+    {
+        auto r1 = sqlite3_open("/spiffs/test3", &db);
+        db_assert_ok(r1);
+    }
+    
+    {
+        static const char*q = R"SQLSQLSQL(
+    CREATE TABLE IF NOT EXISTS user (
+        'id'        INTEGER PRIMARY KEY,
+        'rfid'      BLOB    NOT NULL,
+        'name'      TEXT    NOT NULL,
+        'last_seen' DATE    NOT NULL,
+        'active'    BOOLEAN NOT NULL
+    );
+    )SQLSQLSQL";
+        sqlite3_stmt *stmt;
+        auto r1 = sqlite3_prepare_v2(db, q, -1, &stmt, 0);
+        db_assert_ok(r1);
+        assert(stmt);
+        auto r2 = sqlite3_step(stmt);
+        assert(r2 == SQLITE_DONE);
+        auto r4 = sqlite3_finalize(stmt);
+        db_assert_ok(r4);
+    }
+    {
+        static const char*q = R"SQLSQLSQL(
+    CREATE TABLE IF NOT EXISTS log (
+        "id"        INTEGER PRIMARY KEY,
+        "user_id"   INTEGER NOT NULL,
+        "when"      INTEGER NOT NULL,
+        "type"      CHAR(1) NOT NULL
+    );
+    )SQLSQLSQL";
+        //    FOREIGN KEY(user_id) REFERENCES user(id)  
+        // constraint is nice in theory, but...
+        
+        sqlite3_stmt *stmt;
+        auto r1 = sqlite3_prepare_v2(db, q, -1, &stmt, 0);
+        db_assert_ok(r1);
+        assert(stmt);
+        auto r2 = sqlite3_step(stmt);
+        assert(r2 == SQLITE_DONE);
+        auto r4 = sqlite3_finalize(stmt);
+        db_assert_ok(r4);    
+    }
+    
+    db_set_rtc_offset();
+}
+
+
+
---
+++ rfid_access_control.ino
@@ -0,0 +1,327 @@
+#include <EEPROM.h>
+#include <SPI.h>
+#include <MFRC522.h>
+#include <SPIFFS.h> 
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <WiFiAP.h>
+#include <ESPmDNS.h>
+#include <WebServer.h>
+
+
+typedef uint64_t u64;
+typedef uint32_t u32;
+typedef uint16_t u16;
+typedef uint8_t  u8;
+
+typedef int64_t  s64;
+typedef int32_t  s32;
+typedef int16_t  s16;
+typedef int8_t   s8;
+
+void db_add_user(const char* user_name);
+void db_get_delete_list(char* tmp_buf, size_t tmp_buf_len);
+bool db_check_and_log_access();
+void db_list_log();
+void db_prune();
+void db_del_user(u64 uid);
+void db_init();
+
+
+u64 rtc_offset = 0;
+char tmp_buf[4096];
+u8 rfid[10];
+u8 rfid_len;
+
+
+inline static void debug_print_rfid() {
+    for (int i=0;i<rfid_len;i++) {
+        Serial.printf("%02x ", rfid[i]);
+    }
+    Serial.printf("\n");
+}
+
+const int RST_PIN = 22; // Reset pin
+const int SS_PIN = 21; // Slave select pin
+
+const int PIN_BLUEal  = 15; 
+const int PIN_GREENal = 2; 
+const int PIN_REDal   = 16; 
+const int PIN_UNLOCK  = 4;
+ 
+const int PIN_TOUCH0  = 13; 
+const int PIN_TOUCH1  = 12; 
+const int PIN_TOUCH2  = 14; 
+
+
+MFRC522 mfrc522(SS_PIN, RST_PIN); // Create MFRC522 instance
+
+void rfid_read() {
+  if ( ! mfrc522.PICC_IsNewCardPresent()) {
+    rfid_len = 0;
+    return;
+  }  
+  if ( ! mfrc522.PICC_ReadCardSerial()) {
+    rfid_len = 0;
+    return;
+  }
+  rfid_len = mfrc522.uid.size;
+  memcpy(rfid, mfrc522.uid.uidByte, rfid_len);
+}
+
+
+int unlock_ticks = 0;
+int error_ticks = 0;
+
+bool down0 = 0;
+bool down1 = 0;
+bool down2 = 0;
+int idle_sustain = 0;
+char pin_entry_buffer[10];
+
+int touch0_sustain = 0;
+int touch1_sustain = 0;
+int touch2_sustain = 0;
+
+#define IS_TOUCH(id) __is_touch(id, PIN_TOUCH ## id, touch ## id ##_sustain, down ## id)
+bool __is_touch(int id, int pin, int&sustain, bool&was_down) {
+  bool down = 0; if (touchRead(pin) < 25 /*threshold*/) { 
+    if (sustain < 2) sustain++;
+    else down = 1;   
+  } else sustain = 0;
+  bool pressed = 0; if (down) {
+    if (!was_down) pressed = 1;
+    was_down = 1;
+  } else was_down = 0;
+  return pressed;
+}
+
+
+void setup() {
+  Serial.begin(115200); // Initialize serial communications with the PC
+
+  while (!Serial); // Do nothing if no serial port is opened (added for Arduinos based on ATMEGA32U4)
+  Serial.println("BOOT\n");
+
+
+  auto r1 = SPIFFS.begin(true);
+  assert(r1);
+
+  db_init();
+
+  SPI.begin(); // Init SPI bus
+  mfrc522.PCD_Init(); // Init MFRC522
+  mfrc522.PCD_DumpVersionToSerial(); // Show details of PCD - MFRC522 Card Reader details
+  Serial.println(F("Scan PICC to see UID, SAK, type, and data blocks..."));
+
+  mfrc522.PCD_SetAntennaGain(mfrc522.RxGain_max);
+  //pinMode(5, INPUT_PULLUP);
+  //attachInterrupt(digitalPinToInterrupt(5), rfid_isr, CHANGE);
+
+  pinMode(PIN_BLUEal, OUTPUT);
+  pinMode(PIN_GREENal, OUTPUT);
+  pinMode(PIN_UNLOCK, OUTPUT);
+  pinMode(PIN_REDal, OUTPUT);
+  digitalWrite(PIN_BLUEal, LOW);
+  digitalWrite(PIN_GREENal, HIGH);
+
+
+  pinMode(PIN_TOUCH0, OUTPUT);
+  pinMode(PIN_TOUCH1, OUTPUT);
+  pinMode(PIN_TOUCH2, OUTPUT);
+
+}
+
+
+void pin_entry_buffer_add_char(char c) {
+  memmove(pin_entry_buffer, pin_entry_buffer+1, sizeof pin_entry_buffer-1);
+  pin_entry_buffer[sizeof pin_entry_buffer-1] = c;
+}
+
+enum {
+  STATE_IDLE,
+  STATE_UNLOCK,
+  STATE_PROGRAM,
+} state;
+
+void set_led_red() {
+  digitalWrite(PIN_REDal  , LOW );
+  digitalWrite(PIN_GREENal, HIGH);
+  digitalWrite(PIN_BLUEal , HIGH);
+}
+void set_led_green() {
+  digitalWrite(PIN_REDal  , HIGH);
+  digitalWrite(PIN_GREENal, LOW );
+  digitalWrite(PIN_BLUEal , HIGH);
+}
+void set_led_blue() {
+  digitalWrite(PIN_REDal  , HIGH);
+  digitalWrite(PIN_GREENal, HIGH);
+  digitalWrite(PIN_BLUEal , LOW );
+}
+void set_led_white() {
+  digitalWrite(PIN_REDal  , LOW );
+  digitalWrite(PIN_GREENal, LOW );
+  digitalWrite(PIN_BLUEal , LOW );
+}
+
+
+#define redirect(m_a) server.sendHeader("Location", m_a,true); server.send(302, "text/plain",""); 
+#define set_no_cache() \
+server.sendHeader("Cache-Control", "no-cache, no-store, must-revalidate", true); \
+server.sendHeader("Pragma", "no-cache", true); \
+server.sendHeader("Expires", "0", true); 
+
+
+void loop() {
+  // TODO: if time is not set, then goto maintenance...
+  idle: {
+    Serial.println("IDLE\n");
+
+    // if day_start + 24 hours > now : goto maintenance;
+    
+    for(;;) { 
+      bool t0 = IS_TOUCH(0);
+      bool t1 = IS_TOUCH(1);
+      bool t2 = IS_TOUCH(2);
+      
+      if (t0) { error_ticks=5; pin_entry_buffer_add_char('1'); } 
+      if (t1) { error_ticks=5; pin_entry_buffer_add_char('2'); } 
+      if (t2) { error_ticks=5; pin_entry_buffer_add_char('3'); } 
+      
+      if (!t0 && !t1 && !t2 ) {
+        if (idle_sustain < 100) {
+          idle_sustain++;
+        } else {
+          pin_entry_buffer_add_char('\0');
+        }
+      } else {
+        idle_sustain = 0;
+      } 
+  
+  
+      char pin[] = {'1', '2', '3', '3'};
+    
+      if(memcmp(pin_entry_buffer + sizeof(pin_entry_buffer) - sizeof(pin), pin, sizeof(pin)) == 0) {
+          pin_entry_buffer_add_char('\0');
+          goto unlock;
+      }
+          
+      if(error_ticks>0) {
+        error_ticks--;
+        set_led_red(); 
+      } else {
+        set_led_blue(); 
+      }
+  
+      rfid_read();
+      if (!rfid_len) continue;      
+      Serial.print("UID: ");
+      debug_print_rfid();
+  
+      if (1) {
+        goto unlock;
+      } else {
+        error_ticks = 100;
+      }
+    }
+    delay(1);
+  }
+
+  unlock: {
+    Serial.println("UNLOCK\n");
+    unlock_ticks = 1000;
+    error_ticks = 0;
+      
+    for(;;) { 
+      if (unlock_ticks<=0) {
+        goto idle;
+      }
+      unlock_ticks--;
+      bool t0 = IS_TOUCH(0);
+      if (t0) goto program;
+      digitalWrite(PIN_UNLOCK, unlock_ticks%16!=0);
+          
+      set_led_green();
+      delay(1);  
+    } 
+  }
+
+  program: {
+    Serial.println("PROGRAM\n");
+    WiFi.softAP("access_control", "access_control");
+    MDNS.begin("access_control");
+    WebServer server(80);
+    set_led_white();
+
+    bool should_stop = 0;
+
+    server.onNotFound([&](){
+      redirect("/");
+    });
+    server.on("/del", [&](){
+      db_del_user(atoi(server.arg("id").c_str())); // what happens if arg isn't present? oh well, we just trust the client in program mode...
+      redirect("/delete_list");
+    });
+    server.on("/delete_list", [&](){
+      set_no_cache();
+      db_get_delete_list(tmp_buf, sizeof tmp_buf);
+      server.send(200, "text/html", tmp_buf);
+    });
+    server.on("/", [&](){
+      if (server.method() == HTTP_POST) {
+        int username_arg = -1;
+        for (int i = 0; i< server.args(); i++) {
+          if (server.argName(i) == "username") {
+             username_arg = i; break;
+          }
+        }
+        if (username_arg == -1) {
+          server.send(500, "text/plain", "11111");
+        } else {
+          server.send(200, "text/html", "<h1>Scan new card now</h1>");          
+          for (;;) {
+            rfid_read();
+            if (rfid_len) break;
+
+          }
+          Serial.print("username: ");
+          Serial.println(server.arg(username_arg));
+          Serial.print("UID: ");
+          debug_print_rfid();
+          db_add_user(server.arg(username_arg));
+          ESP.restart();
+
+
+        }
+      } else {
+        server.send(200, "text/html", 
+        "<p><a href=/view_access>View recent accesses</a></p>"
+        "<p><a href=/delete_list>Delete Users</a></p>"
+        "<p><form method=post><h1>Enter name for new person</h1>"
+        "<input type=text name=username /><input type=submit /></p>"
+        );
+      }
+    });
+
+    server.begin();
+
+    for(;;) {
+      server.handleClient();
+      bool t0 = IS_TOUCH(0);
+      if (t0) goto idle;
+    }
+
+  }
+
+  maintenence: {
+    // TODO: Swap log files, 
+    // TODO: Log onto wifi
+    // TODO: get time with NTP, set it
+    // TOOD: Delete old users
+    // TODO: Save time...
+    ESP.restart();
+    
+  }
+  
+}
